73ea801151ddc6d556d8040f26e1620c0052694c
Martin Bruno <mb.martinbruno@gmail.com>
2021-09-24 12:04:21 +0200

    fix deployment issues

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 // Define port
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 // Import routes
 app.use("/api/users", require("./routes/users"));
@@ -29,6 +29,6 @@ app.use("/api/projects", require("./routes/projects"));
 app.use("/api/tasks", require("./routes/tasks"));
 
 // Run and listen server at PORT
-app.listen(PORT, () => {
-  console.log(`Server is running at port ${PORT}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running at port ${port}`);
 });
